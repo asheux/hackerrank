17c9563aa876882a9c8c13c37902c9010e289a7c
asheuh <asheuh49@gmail.com>
2019-11-16 19:03:11 +0300

    not so perfect solution

--- container-balls.py
+++ container-balls.py
@@ -17,10 +17,19 @@ def organizingContainers(container):
     m1 = container
     result = ""
 
+#     for c in range(len(m1)):
+#         nc = (c + 1) % len(m1)
+    counts = {c: 0 for c in range(len(m1))}
+
+    for cx in range(len(m1)):
+        for tx in range(len(m1[cx])):
+            counts[cx] += m1[tx][cx]
+
     for c in range(len(m1)):
-        nc = (c + 1) % len(m1)
         t = c
+        nc = (c + 1) % len(m1[c])
         nt = nc
+
         if m1[nc][t] > 0 < m1[c][nt]:
             if m1[nc][t] < m1[c][nt]:
                 m1[c][nt] -= m1[nc][t]
@@ -37,26 +46,28 @@ def organizingContainers(container):
                 m1[c][t] += temp
                 m1[nc][nt] += temp
                 m1[c][nt], m1[nc][t] = 0, 0
-        print(c, nc, m1[c])
 
-        # sub[c] = {i: value for i, value in enumerate(m1[c])}
+    for i, v in enumerate(m1):
+        s = sum(v)
+        if s == counts[i]:
+            result = "Possible"
+        else:
+            result = "Impossible"
+    print(result)
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    q = int(input())
+    q = int(input('Enter number of queries: '))
 
     for q_itr in range(q):
-        n = int(input())
+        n = int(input('Enter number of rows and column: '))
 
         container = []
 
-        for _ in range(n):
-            container.append(list(map(int, input().rstrip().split())))
+        for i in range(n):
+            container.append(list(map(int, input(f'Enter {n} row {i} values sep by space: ').rstrip().split())))
 
         result = organizingContainers(container)
 
-        fptr.write(result + '\n')
-
-    fptr.close()
-
+        print(result)
